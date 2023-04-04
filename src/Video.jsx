eed403c2325c8d893e84337e19b59e3982299287
aaronPeter <aaronpeter@pop-os.localdomain>
2023-04-04 11:17:54 +0300

    added the list fn so that it shows the list of the links

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Video() {
-  const handleClick = (e) => {
-    e.preventDefault();
+  const [inputValue, setInputValue] = useState("");
+  const [listItems, setListItems] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setListItems([...listItems, inputValue]);
+    // Do something with the input value, e.g. submit a form
+    console.log(inputValue);
+
+    setInputValue("");
   };
   return (
     <div>
       <h4>Video Downloader</h4>
       <h1>Video Downloader</h1>
-      <input type="text" />
-      <button onClick={handleClick}></button>
+      <p>a safe and easy YouTube Downloader</p>
+      {/* <p id="line2">paste link here</p> */}
+      <input
+        type="text"
+        placeholder="paste link here"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleButtonClick}> DownLoad</button>
+      {listItems.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
 }
